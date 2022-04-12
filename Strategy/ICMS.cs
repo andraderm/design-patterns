353933b1fea2bfbd8bae8557a523499c826d5cda
Rodrigo Andrade <rodrigom.andrade95@gmail.com>
2022-04-12 09:58:40 -0300

    Initial commit - Strategy design pattern

---
+++ Strategy/ICMS.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+{
+    public class ICMS : Imposto
+    {
+        public double Calcula(Orcamento orcamento) => orcamento.Valor * 0.05;
+    }
+}
---
+++ Strategy/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Strategy;
+
+Imposto iss = new ISS();
+Imposto icsm = new ICMS();
+Imposto iccc = new ICCC();
+
+Orcamento orcamento = new Orcamento(50);
+
+CalculadorDeImposto calculador = new CalculadorDeImposto();
+
+calculador.RealizaCalculo(orcamento, iss);
+calculador.RealizaCalculo(orcamento, icsm);
+calculador.RealizaCalculo(orcamento, iccc);
+
+Orcamento orcamento2 = new Orcamento(1450);
+Orcamento orcamento3 = new Orcamento(5000);
+
+calculador.RealizaCalculo(orcamento2, iccc);
+calculador.RealizaCalculo(orcamento3, iccc);

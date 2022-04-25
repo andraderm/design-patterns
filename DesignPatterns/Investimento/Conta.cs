ef1c1120c5b5a71b603694a902756edf8eaa48e9
Rodrigo Andrade <rodrigom.andrade95@gmail.com>
2022-04-25 11:07:00 -0300

    Added reports

--- DesignPatterns/Investimento/Conta.cs
+++ DesignPatterns/Investimento/Conta.cs
@@ -10,17 +10,41 @@ namespace Investimento
     {
         public string? Titular { get; set; }
         public double Valor { get; set; }
+        public string Nome { get; set; }
+        public string Numero { get; set; }
+        public string Agencia { get; set; }
+        public decimal Saldo { get; set; }
 
         public Conta() {}
+
         public Conta(double valor)
         {
             Valor = valor;
+            Saldo = (decimal)valor;
+            Titular = "";
+            Nome = "";
+            Numero = "";
+            Agencia = "";
         }
 
         public Conta(string titular, double valor)
         {
             Titular = titular;
             Valor = valor;
+            Nome = "";
+            Saldo = (decimal)valor;
+            Numero = "";
+            Agencia = "";
+        }
+
+        public Conta(string titular, double valor, string nome, string numero, string agencia)
+        {
+            Titular = titular;
+            Valor = valor;
+            Nome = nome;
+            Saldo = (decimal)valor;
+            Numero = numero;
+            Agencia = agencia;
         }
 
         public void AdicionaSaldo(double valor) => Valor += valor;
